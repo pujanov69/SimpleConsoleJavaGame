045a6deed77b1aabd5bd5e18b50c83e6ef410d79
yoomes <yoomesbhujel@gmail.com>
2020-07-20 22:32:40 +0545

    added exception and file reader

---
+++ ConfigurationFormatException.java
@@ -0,0 +1,8 @@
+
+public class ConfigurationFormatException extends RuntimeException {
+	private static final long serialVersionUID = 8747202975776093424L;
+
+	public ConfigurationFormatException(String msg) {
+		super(msg);
+	}
+}
