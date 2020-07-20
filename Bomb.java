984c0f6b0f11a25da565696a44164e46dd5e9377
pujanov69 <pujanov69@gmail.com>
2020-07-20 21:18:45 +0545

    added Class Bomb

---
+++ Bomb.java
@@ -0,0 +1,16 @@
+
+public class Bomb extends Weapon {
+	
+
+
+	private int numberOfEnemies;
+
+	
+	public Bomb(String name, int low, int high, int numberOfEnemies) {
+		super(name, low, high);
+		this.numberOfEnemies = numberOfEnemies;
+		// TODO Auto-generated constructor stub
+	}
+
+
+}
